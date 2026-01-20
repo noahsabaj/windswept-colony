e92b4c297ad6720288e98fabc65510a42f9e0069
Noah Sabaj <noahsabaj@gmail.com>
2026-01-19 22:53:24 -0500

    Disable faction whitelist requirements to allow open transfers without prior approval

--- schema/sv_schema.lua
+++ schema/sv_schema.lua
@@ -2,4 +2,13 @@
     Windswept Colony RP - Server Schema
 ]]--
 
--- Server-side schema code goes here
+-- Disable faction whitelist requirements
+-- All factions are open for transfer without needing /PlyWhitelist first
+-- Players still start as Civilians (the only isDefault=true faction)
+-- but admins can /PlyTransfer them to any faction directly
+local playerMeta = FindMetaTable("Player")
+
+function playerMeta:HasWhitelist(faction)
+    -- Always return true - no whitelist restrictions
+    return true
+end
